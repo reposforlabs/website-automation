4cea2b864584b4cf287102d61cfe522f09044664
reposforlabs <160736679+reposforlabs@users.noreply.github.com>
2024-11-19 19:29:23 +0530

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 sudo apt-get update
-sudo apt-get install -y apache2 php git
+sudo apt-get install apache2 php git -y
 sudo systemctl enable apache2.service
-sudo service apache2 start
-sudo git clone https://github.com/teachmycloud/apache-automation
-sudo cp ./apache-automation/* /var/www/html
+sudo systemctl start apache2
+sudo git clone https://github.com/reposforlabs/website-automation.git
+sudo cp ./website-automation/* /var/www/html
 sudo mv /var/www/html/htaccess /var/www/html/.htaccess
-sed -i '19iDirectoryIndex index.php /html/index.php' /etc/apache2/sites-available/000-default.conf 
-sudo service apache2 restart
+sudo sed -i '19iDirectoryIndex index.php /html/index.php' /etc/apache2/sites-available/000-default.conf 
+sudo systemctl restart apache2
 
